bbd79ae088f21c62d96f14e65ac9dec95d8aa390
Jon Carlson <jon@jomca.co>
2014-07-31 17:00:15 -0600

    Refactored translation function

--- controllers/index.js
+++ controllers/index.js
@@ -9,19 +9,9 @@ var indexController = {
 		res.render('index');
 	},
 	translateText: function(req, res) {
-		console.log(req.body);
-		beglobal.translations.translate(
-			req.body,
-			// {text: req.body.text, from: req.body.from, to: req.body.to},
-			function(err, results) {
-			if (err) {
-				console.log('Translation error: ', err);
-				res.status(500).end('No translation found');
-			}
-			else {
-				res.status(200).end(results.translation);
-			}
-  		});
+		// console.log(req.body);
+		
+		translateWordSend(req.body, req, res);
 
 		// console.log('translate func fired');
 		// res.render('translate');
@@ -35,6 +25,31 @@ var indexController = {
 	translate: function(req, res) {
 		res.render('translate');
 	}
+};
+
+var translateWordSend = function(translationObject, req, res) {
+
+	beglobal.translations.translate(translationObject, function(err, results) {
+		if (err) {
+			console.log('Translation error: ', err);
+			res.status(500).end('No translation found');
+		}
+		else {
+			res.status(200).end(results.translation);
+		}
+	});
+};
+
+var translateWordValue = function(translationObject, req, res) {
+
+	beglobal.translations.translate(translationObject, function(err, results) {
+		if (err) {
+			console.log('Translation error: ', err);
+		}
+		else {
+			return results.translation;
+		}
+	});
 };
 
 module.exports = indexController;
